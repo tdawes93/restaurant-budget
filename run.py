5f78443f8215f737c2a7d830dba6e21eacae5357
tdawes93 <tdawes136@googlemail.com>
2022-02-04 22:46:33 +0000

    Add function structure to run.py

--- run.py
+++ run.py
@@ -10,3 +10,22 @@ SCOPE = [
 CREDS = (Credentials.from_service_account_file("creds.json"))
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 SHEET = gspread.authorize(SCOPED_CREDS).open("restaurant_budget")
+
+def input_bookings():
+
+def add_bookings_to_spreadsheet():
+
+def calculate_walkins():
+
+def add_walkins_to_spreadsheet():
+
+def calculate_takings():
+
+def add_takings_to_spreadsheet():
+
+def calculate_staff_numbers():
+
+def add_staff_numbers_to_spreadsheet():
+
+def main():
+    
