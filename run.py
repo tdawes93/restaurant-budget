d35b63e410155bf4563fb6826a0202ce51e3a608
tdawes93 <tdawes136@googlemail.com>
2022-02-06 20:24:01 +0000

    Add code to input_bookings function to collect the inputted data from the user

--- run.py
+++ run.py
@@ -11,21 +11,39 @@ CREDS = (Credentials.from_service_account_file("creds.json"))
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 SHEET = gspread.authorize(SCOPED_CREDS).open("restaurant_budget")
 
+# bookings = SHEET.worksheet("bookings")
+# data = bookings.get_all_values()
+# print(data)
+
+
 def input_bookings():
+    """
+    Get bookings figures inputted by user
+    """
+    print("Please enter your booking numbers for the week, Monday to Sunday")
+    print("Data must be in the form of 7 numbers seperated by commas\n")
 
-def add_bookings_to_spreadsheet():
+    bookings_input = input("Enter your data here: ")
+    print(f"\nYou have entered your weekly booking numbers as {bookings_input}")
+    bookings = [int(booking) for booking in bookings_input.split(",")]
+    print(bookings)
 
-def calculate_walkins():
 
-def add_walkins_to_spreadsheet():
+# def add_bookings_to_spreadsheet():
 
-def calculate_takings():
+# def calculate_walkins():
 
-def add_takings_to_spreadsheet():
+# def add_walkins_to_spreadsheet():
 
-def calculate_staff_numbers():
+# def calculate_takings():
 
-def add_staff_numbers_to_spreadsheet():
+# def add_takings_to_spreadsheet():
 
-def main():
-    
+# def calculate_staff_numbers():
+
+# def add_staff_numbers_to_spreadsheet():
+
+# def main():
+
+input_bookings()
+
